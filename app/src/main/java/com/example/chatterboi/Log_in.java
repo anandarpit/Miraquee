eacb3db5e3abe4b62f90bea6b0e6a030c286cdf7
Shivam <testzapofficial@gmail.com>
2020-10-13 23:50:35 +0530

    login

--- app/src/main/java/com/example/chatterboi/Log_in.java
+++ app/src/main/java/com/example/chatterboi/Log_in.java
@@ -4,15 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.rengwuxian.materialedittext.MaterialEditText;
@@ -24,6 +30,8 @@ public class Log_in extends AppCompatActivity {
     TextView forgotpassword;
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
+    Button log;
+    MaterialEditText email, pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +40,9 @@ public class Log_in extends AppCompatActivity {
         firebaseAuth = firebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
         forgotpassword = findViewById(R.id.forgot_password);
+       log =  findViewById(R.id.login);
+       email= findViewById(R.id.login_email);
+        pass= findViewById(R.id.login_password);
         forgotpassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View mview) {
@@ -77,5 +88,48 @@ public class Log_in extends AppCompatActivity {
 
 
             }});
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String Email=email.getText().toString().trim();
+                final String Pass=pass.getText().toString().trim();
+                if(TextUtils.isEmpty(Email)) {
+
+                    email.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(Pass))
+                {
+                    pass.setError("Password is Required");
+                    return;
+                }
+                if (Pass.length()<7)
+                {
+                    pass.setError("Password must contain atleast 8 characters");
+                    return;
+                }
+                firebaseAuth.signInWithEmailAndPassword(Email,Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            FirebaseUser users=firebaseAuth.getCurrentUser();
+                            if (users.isEmailVerified()) {
+                                Toast.makeText(Log_in.this, "Login Sucessful!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Log_in.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(Log_in.this, "Email not Verified", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                            Toast.makeText(Log_in.this, "Login Failed!"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
             }
 }
